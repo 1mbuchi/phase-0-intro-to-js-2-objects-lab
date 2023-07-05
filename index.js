d56a21170d26c1d25df6df457852f89e20127f37
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-07-05 23:46:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 let employee = {
-    name: 'John Doe',
-    streetAddress: '123 Main St'
+  name: 'John Doe',
+  streetAddress: '123 Main St'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
   };
+}
 
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return {
-      ...employee,
-      [key]: value
-    };
-  }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
 
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-  }
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
 
-  function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
-    delete newEmployee[key];
-    return newEmployee;
-  }
-
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
-  }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
